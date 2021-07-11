93c3b84fee3801a56c234bbf870f38faaa73845f
Thomas Min <thomas.min.git@gmail.com>
2021-07-11 12:56:56 +0900

    feat: initial commit

---
+++ my_lib/tests/lib_test.rs
@@ -0,0 +1,7 @@
+#[cfg(test)]
+mod lib_test {
+    #[test]
+    fn test_add() {
+        assert_eq!(my_lib::add(2, 2), 4);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use my_lib;
+
+fn main() {
+    println!("Hello, world!");
+    println!("2+2={:?}", my_lib::add(2, 2));
+}
